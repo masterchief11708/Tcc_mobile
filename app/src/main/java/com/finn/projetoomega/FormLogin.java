d72cbd49b4b9973261c07f90a315c025e1619fa0
masterchief11728 <rm87155@estudante.fieb.edu.br>
2024-06-20 08:50:45 -0300

    Tela de Login 1.0: scroll

--- app/src/main/java/com/finn/projetoomega/FormLogin.java
+++ app/src/main/java/com/finn/projetoomega/FormLogin.java
@@ -4,12 +4,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class FormCadastro extends AppCompatActivity {
+public class FormLogin extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_form_cadastro);
+        setContentView(R.layout.activity_form_login);
 
         getSupportActionBar().hide();
     }
